85be05570fbf32a10dd74f631f8aec557161cfa9
A.mamdouh <84878309+darwiiiish@users.noreply.github.com>
2024-05-24 11:34:32 +0300

    Add files via upload

---
+++ Frontend/HTML & CSS/login_b.js
@@ -0,0 +1,11 @@
+import express from'express'
+import bcrypt from 'bcrypt'
+
+const app = express()
+const port = 3000;
+const users = [];
+
+app.use(express.json())
+app.listen(port,()=>{
+    console.log('ANA gEEEEEEETTTTTTTTTTTTTTT');
+})
